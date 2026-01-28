24536127d640dd3ef9c0ad1b83312aa3e06a900e
Dominic juma <jumadomi518@gmail.com>
2026-01-28 16:07:09 +0300

    m

--- src/authentication/passport.js
+++ src/authentication/passport.js
@@ -11,7 +11,7 @@
  passport.use(
  new LocalStrategy({ usernameField: "email"}, async (email, password, done) => {
   try {
-   const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [email]);
+   const { rows } = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
    const user = rows[0];
   if (!user) {
   return done(null, false);
